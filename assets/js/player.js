28a71c5565cd930e63ab1d93fe807826428f8e0b
owky <owky2413@gmail.com>
2021-12-05 22:55:09 +0900

    Add 'play' 'pause' 'backward' 'forward' control.

--- assets/js/player.js
+++ assets/js/player.js
@@ -12,12 +12,46 @@ var player;
 function onYouTubeIframeAPIReady() {
 	player = new YT.Player('player', {
 	  videoId: video_id,
-	  events: {}
+	  events: {
+		  'onStateChange': changePlayerIcon
+	  }
 	});
+}
+
+function changePlayerIcon (event) {
+	switch (event.data) {
+		case 0:
+			document.getElementById("playerStatus").className = "fas fa-2x fa-play";
+			break;
+		case 1:
+			document.getElementById("playerStatus").className = "fas fa-2x fa-pause";
+			break;
+		case 2:
+			document.getElementById("playerStatus").className = "fas fa-2x fa-play";
+			break;
+	}
 }
 
 function backToTop () {
 	player.seekTo(0);
+}
+
+function backward () {
+	player.seekTo(player.getCurrentTime() - 10);
+}
+
+function forward () {
+	player.seekTo(player.getCurrentTime() + 10);
+}
+
+function play() {
+	if (player.getPlayerState() == 1) {
+		player.pauseVideo();
+		document.getElementById("playerStatus").className = "fas fa-2x fa-play";
+	} else {
+		player.playVideo();
+		document.getElementById("playerStatus").className = "fas fa-2x fa-pause";
+	}
 }
 
 function changeVideo () {
