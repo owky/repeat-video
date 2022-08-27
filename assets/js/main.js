5d75a582cc45434c23259edb00d4c705dffed075
owky <owky2413@gmail.com>
2022-08-27 14:01:42 +0000

    YouTube iFrame APIの操作をmain.jsに寄せる

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,36 @@
-dataManager = new DataManager();
+var dataManager = new DataManager();
+var video_id = null;
+var player;
+
+window.onload = function () {
+    // YouTube iFrame APIのロード
+	var tag = document.createElement('script');
+	tag.src = "https://www.youtube.com/iframe_api";
+	var firstScriptTag = document.getElementsByTagName('script')[0];
+	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+	showPlayList();
+}
+
+function onYouTubeIframeAPIReady() {
+	player = new YT.Player('player', {
+	  videoId: video_id,
+	  events: {
+		  'onStateChange': changePlayerIcon
+	  }
+	});
+}
+
+function changeVideo (vid, title, thumbnail_url) {
+	if (vid) video_id = vid;
+	player.loadVideoById(video_id);
+	if (h = dataManager.playList[video_id]) {
+		document.getElementById("speedSlider").value = h['speed'] * 100;
+		changeSpeed()
+	}
+	dataManager.addPlayList(video_id, title, thumbnail_url);
+	showPlayList();
+}
 
 function switchTab(target) {
     document.getElementById("play_list").style.display = "none";
