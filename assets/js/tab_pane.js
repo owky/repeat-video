92ff9a06d538900ac7090918b028fa2f8106b1dd
Yuhei Ohwaki <yuhei.ohwaki@optim.co.jp>
2022-12-06 22:39:25 +0900

    リファクタリング

--- assets/js/tab_pane.js
+++ assets/js/tab_pane.js
@@ -1,20 +1,25 @@
 class TabPane {
   constructor() {
+    this.search_tab = document.getElementById("search-tab");
+    this.search_pane = document.getElementById("search-pane");
+    this.play_list_tab = document.getElementById("play-list-tab");
+    this.play_list_pane = document.getElementById("play-list-pane");
+
     document.getElementById("play-list-tab").addEventListener('click', this.playList.bind(this));
     document.getElementById("search-tab").addEventListener('click', this.search.bind(this));
   }
 
   playList() {
-    document.getElementById("search-pane").style.display = "none";
-    document.getElementById("search-tab").removeAttribute("class");
-    document.getElementById("play-list-pane").style.display = "block";
-    document.getElementById("play-list-tab").setAttribute("class", "is-active");
+    this.search_pane.style.display = "none";
+    this.search_tab.removeAttribute("class");
+    this.play_list_pane.style.display = "block";
+    this.play_list_tab.setAttribute("class", "is-active");
   }
 
   search() {
-    document.getElementById("play-list-pane").style.display = "none";
-    document.getElementById("play-list-tab").removeAttribute("class");
-    document.getElementById("search-pane").style.display = "block";
-    document.getElementById("search-tab").setAttribute("class", "is-active");
+    this.play_list_pane.style.display = "none";
+    this.play_list_tab.removeAttribute("class");
+    this.search_pane.style.display = "block";
+    this.search_tab.setAttribute("class", "is-active");
   }
 }
